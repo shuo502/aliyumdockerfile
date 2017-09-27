9a28daa64df69d98ef312c8c90824de5063269e5
Holger Knust <holger@knust.us>
2017-09-26 22:00:41 -0400

    created separate Flask app module

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello OpenShift World!"
+from helloworld import app
 
 if __name__ == "__main__":
     app.run()
